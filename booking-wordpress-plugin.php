bbbf759bf2264e7236c48762bfa5ebe3dd310771
Ziga Vukcevic <ziga@be-codified.com>
2017-10-06 21:08:47 +0200

    Try to create table

--- booking-wordpress-plugin.php
+++ booking-wordpress-plugin.php
@@ -9,8 +9,23 @@
    License: GPL2
    */
 
-  function bwp_activate() {
-    var_dump('activated');
+  function bwp_prepare_table() {
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+  	$table_name = $wpdb->prefix . 'bwp';
+
+    $sql = "CREATE TABLE $table_name (
+      id int(9) NOT NULL AUTO_INCREMENT,
+      url varchar(55) DEFAULT '' NOT NULL,
+      PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+    echo $wpdb->last_error;
+    die();
   }
 
   function bwp_deactivate() {
@@ -24,6 +39,6 @@
   // Using hooks to register plugin function to be run on plugin
   // activation, deactivation and uninstall
 
-  register_activation_hook(__FILE__, 'bwp_activate');
+  register_activation_hook(__FILE__, 'bwp_prepare_table');
   register_deactivation_hook(__FILE__, 'bwp_deactivate');
   register_uninstall_hook(__FILE__, 'bwp_unistall');
