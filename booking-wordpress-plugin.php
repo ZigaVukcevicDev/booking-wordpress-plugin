4adb65ae330790388c540bbd767a59504f55469d
Ziga Vukcevic <ziga@be-codified.com>
2017-10-06 20:02:00 +0200

    Add plugin desc

---
+++ booking-wordpress-plugin.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Booking Wordpress Plugin
+   Plugin URI: http://www.booking-wordpress-plugin.com
+   Description: Plugin capable of request booking through simple form.
+   Version: 1.0
+   Author: Žiga Vukčevič
+   Author URI: http://www.be-codified.com
+   License: GPL2
+   */
+?>
