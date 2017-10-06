499edb3f9f1aca8545280755d9c61cd2356c48bc
Ziga Vukcevic <ziga@be-codified.com>
2017-10-06 20:23:40 +0200

    Add basic function & hooks

--- booking-wordpress-plugin.php
+++ booking-wordpress-plugin.php
@@ -8,4 +8,22 @@
    Author URI: http://www.be-codified.com
    License: GPL2
    */
-?>
+
+  function bwp_activate() {
+    var_dump('activated');
+  }
+
+  function bwp_deactivate() {
+    var_dump('deactivated');
+  }
+
+  function bwp_unistall() {
+    var_dump('uninstall');
+  }
+
+  // Using hooks to register plugin function to be run on plugin
+  // activation, deactivation and uninstall
+
+  register_activation_hook(__FILE__, 'bwp_activate');
+  register_deactivation_hook(__FILE__, 'bwp_deactivate');
+  register_uninstall_hook(__FILE__, 'bwp_unistall');
